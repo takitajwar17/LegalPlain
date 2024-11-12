e6375b2253a3db1d26482afb29d17449fe53cb7f
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-11-12 18:33:06 +0600

    Add Vercel Analytics

--- Landing Page/app/layout.tsx
+++ Landing Page/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
